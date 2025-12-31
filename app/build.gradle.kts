75decb716453666c83a6fdd4c18b67ec76e07cd1
MohamedMBG <mohamedbaghdad.me@gmail.com>
2025-12-31 23:47:06 +0100

    enabling viewbinding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.adminloyalty"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.adminloyalty"
         minSdk = 24
